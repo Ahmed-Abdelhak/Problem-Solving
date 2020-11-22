d95b74464102aa61335669dde32cc21bb58b3950
Ahmed Abdelhak <engineer.ahmedabdelhak@gmail.com>
2020-11-22 23:09:39 +0200

    BinarySeatch_while_loop

--- Algorithms/BinarySearch/BinarySearch.py
+++ Algorithms/BinarySearch/BinarySearch.py
@@ -5,11 +5,11 @@ class BinarySearch:
             self.__left = 0
             self.__right = len(self.__items) - 1
 
-    def serach(self):
-          return self.__search(self.__el,self.__items,self.__left,self.__right)
+    def serach_recursive(self):
+          return self.__search_recursive(self.__el,self.__items,self.__left,self.__right)
 
     # acts as private Method
-    def __search(self, el , items, left , right):
+    def __search_recursive(self, el , items, left , right):
       # edge Case 
       # what if passing the element and not found ?  the arr will be shrinked to 0 el
       # in other words {right = arr.length-1 = -1}  , {left = 0}
@@ -19,9 +19,28 @@ class BinarySearch:
       # base case
       if el == items[mid]: return el
       if el < items[mid]:
-            return self.__search(el,items,left, mid-1)
+            return self.__search_recursive(el,items,left, mid-1)
       
-      return self.__search(el, items, mid+1, right)
+      return self.__search_recursive(el, items, mid+1, right)
 
+
+    def search(self, el, arr):
+          left = self.__left
+          right = self.__right
+            
+          while( left <= right):
+                mid = (left + right) // 2
+                if el < arr[mid] :
+                      right = mid -1
+                elif el > arr[mid]:
+                      left = mid -1
+                else:
+                      return arr[mid]
+                  
+          return -1
+          
+          
       
-print(BinarySearch(3, [1, 2, 3, 4, 5]).serach())  # elements must be sorted
+            
+      
+print(BinarySearch(3, [1, 2, 3, 4, 5]).serach_recursive())  # elements must be sorted
